50d6c2a5dec28041b91c357d3e020b8f9017bd35
SyedTalha71x <syedtalha71x@gmail.com>
2025-01-02 17:51:26 +0500

    updates

---
+++ app/welcome/welcome.tsx
@@ -0,0 +1,9 @@
+export function Welcome() {
+  return (
+    <main className="flex justify-center items-center p-20">
+      <div className="text-white font-extrabold text-4xl">
+        Learning React Router v7
+      </div>
+    </main>
+  );
+}
